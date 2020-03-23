9dddf8f15ab4d71f13a1d91ba54ee976a6da7876
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-03-23 10:25:33 +0200

    myAccount

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -1,7 +1,6 @@
 package com.bookstore.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,7 +10,7 @@ public class HomeController {
 		return "index";
 	}
 
-	@GetMapping("/myAccount")
+	@RequestMapping("/myAccount")
 	public String myAccount() {
 		return "myAccount";
 	}
