3e2bf5f5f082ee74ab6e354b74659cb8afc4ed0c
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-03-28 14:45:26 +0200

    Security Config

--- src/main/java/com/bookstore/bookstore/DemoApplication.java
+++ src/main/java/com/bookstore/bookstore/DemoApplication.java
@@ -1,12 +1,15 @@
 package com.bookstore.bookstore;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @ComponentScan("com.bookstore.controller")
-@ComponentScan("com.bookstore.domain")
+@EntityScan("com.bookstore.domain")
 public class DemoApplication {
 
 	public static void main(String[] args) {
