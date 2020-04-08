97e3acb444009d4e2f0cab8c87f0b3bd7e45141f
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-04-08 21:59:43 +0200

    Optimized

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -1,13 +1,10 @@
-package com.bookstore.bookstore;
+package com.bookstore;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -21,19 +18,13 @@ import com.bookstore.utility.SecurityUtility;
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
-@ComponentScan("com.bookstore.controller")
-@ComponentScan("com.bookstore.service")
-@ComponentScan("com.bookstore.utility")
-@ComponentScan("com.bookstore")
-@EntityScan("com.bookstore.domain")
-@EnableJpaRepositories("com.bookstore.repository")
-public class DemoApplication implements CommandLineRunner {
+public class BookstoreApplication implements CommandLineRunner {
 
 	@Autowired
 	private UserService userService;
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
 	@Override
--- src/main/java/com/bookstore/domain/User.java
+++ src/main/java/com/bookstore/domain/User.java
@@ -23,6 +23,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class User implements UserDetails {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false, updatable = false)
