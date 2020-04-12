0b56d3ca635ad7550e4964bbe2b92de68eeeca30
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-04-12 04:02:06 +0200

    bookshelf working

---
+++ src/main/java/com/bookstore/service/BookService.java
@@ -0,0 +1,14 @@
+package com.bookstore.service;
+
+import java.util.List;
+
+import com.bookstore.domain.Book;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface BookService {
+
+    List<Book> findAll();
+
+}
