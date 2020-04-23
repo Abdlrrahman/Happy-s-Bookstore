56a6088f1fe07cf5b96bb695072e4d67dac4a923
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-04-23 09:51:46 +0200

    UserPayment

---
+++ src/main/java/com/bookstore/domain/UserPayment.java
@@ -0,0 +1,126 @@
+package com.bookstore.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class UserPayment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String type;
+    private String cardName;
+    private String cardNumber;
+    private int expiryMonth;
+    private int expiryYear;
+    private int cvc;
+    private String holderName;
+    private boolean defaultPayment;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "userPayment")
+    private UserBilling userBilling;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCardName() {
+        return this.cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String getCardNumber() {
+        return this.cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public int getExpiryMonth() {
+        return this.expiryMonth;
+    }
+
+    public void setExpiryMonth(int expiryMonth) {
+        this.expiryMonth = expiryMonth;
+    }
+
+    public int getExpiryYear() {
+        return this.expiryYear;
+    }
+
+    public void setExpiryYear(int expiryYear) {
+        this.expiryYear = expiryYear;
+    }
+
+    public int getCvc() {
+        return this.cvc;
+    }
+
+    public void setCvc(int cvc) {
+        this.cvc = cvc;
+    }
+
+    public String getHolderName() {
+        return this.holderName;
+    }
+
+    public void setHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+
+    public boolean isDefaultPayment() {
+        return this.defaultPayment;
+    }
+
+    public boolean getDefaultPayment() {
+        return this.defaultPayment;
+    }
+
+    public void setDefaultPayment(boolean defaultPayment) {
+        this.defaultPayment = defaultPayment;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public UserBilling getUserBilling() {
+        return this.userBilling;
+    }
+
+    public void setUserBilling(UserBilling userBilling) {
+        this.userBilling = userBilling;
+    }
+
+}
