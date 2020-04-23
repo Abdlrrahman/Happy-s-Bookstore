ca609402e662a0f6942008dcb33e84fb98797d3a
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-04-23 10:01:19 +0200

    User Billing

---
+++ src/main/java/com/bookstore/domain/UserBilling.java
@@ -0,0 +1,99 @@
+package com.bookstore.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class UserBilling {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String userBillingName;
+    private String userBillingStreet1;
+    private String userBillingStreet2;
+    private String userBillingCity;
+    private String userBillingState;
+    private String userBillingCountry;
+    private String userBillingZipcode;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private UserPayment userPayment;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserBillingName() {
+        return this.userBillingName;
+    }
+
+    public void setUserBillingName(String userBillingName) {
+        this.userBillingName = userBillingName;
+    }
+
+    public String getUserBillingStreet1() {
+        return this.userBillingStreet1;
+    }
+
+    public void setUserBillingStreet1(String userBillingStreet1) {
+        this.userBillingStreet1 = userBillingStreet1;
+    }
+
+    public String getUserBillingStreet2() {
+        return this.userBillingStreet2;
+    }
+
+    public void setUserBillingStreet2(String userBillingStreet2) {
+        this.userBillingStreet2 = userBillingStreet2;
+    }
+
+    public String getUserBillingCity() {
+        return this.userBillingCity;
+    }
+
+    public void setUserBillingCity(String userBillingCity) {
+        this.userBillingCity = userBillingCity;
+    }
+
+    public String getUserBillingState() {
+        return this.userBillingState;
+    }
+
+    public void setUserBillingState(String userBillingState) {
+        this.userBillingState = userBillingState;
+    }
+
+    public String getUserBillingCountry() {
+        return this.userBillingCountry;
+    }
+
+    public void setUserBillingCountry(String userBillingCountry) {
+        this.userBillingCountry = userBillingCountry;
+    }
+
+    public String getUserBillingZipcode() {
+        return this.userBillingZipcode;
+    }
+
+    public void setUserBillingZipcode(String userBillingZipcode) {
+        this.userBillingZipcode = userBillingZipcode;
+    }
+
+    public UserPayment getUserPayment() {
+        return this.userPayment;
+    }
+
+    public void setUserPayment(UserPayment userPayment) {
+        this.userPayment = userPayment;
+    }
+
+}
