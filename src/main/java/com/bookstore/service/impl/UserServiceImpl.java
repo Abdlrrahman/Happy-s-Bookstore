7ed22514270220e126abe37a78921b05a2387ce1
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-05-06 19:33:33 +0200

    shopping cart fixed

--- src/main/java/com/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/bookstore/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -66,7 +65,6 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    @Transactional
     public User createUser(User user, Set<UserRole> userRoles) throws Exception {
         User localUser = userRepository.findByUsername(user.getUsername());
 
