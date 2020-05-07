362207eb8a3afe69037bd0d01ce6967274fe426c
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-05-07 07:49:33 +0200

    addItem path

--- src/main/java/com/bookstore/controller/ShoppingCartController.java
+++ src/main/java/com/bookstore/controller/ShoppingCartController.java
@@ -3,9 +3,11 @@ package com.bookstore.controller;
 import java.security.Principal;
 import java.util.List;
 
+import com.bookstore.domain.Book;
 import com.bookstore.domain.CartItem;
 import com.bookstore.domain.ShoppingCart;
 import com.bookstore.domain.User;
+import com.bookstore.service.BookService;
 import com.bookstore.service.CartItemService;
 import com.bookstore.service.ShoppingCartService;
 import com.bookstore.service.UserService;
@@ -13,6 +15,7 @@ import com.bookstore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -28,6 +31,9 @@ public class ShoppingCartController {
     @Autowired
     private ShoppingCartService shoppingCartService;
 
+    @Autowired
+    private BookService bookService;
+
     @RequestMapping("/cart")
     public String shoppingCart(Model model, Principal principal) {
         User user = userService.findByUsername(principal.getName());
@@ -41,6 +47,23 @@ public class ShoppingCartController {
         model.addAttribute("shoppingCart", shoppingCart);
 
         return "shoppingCart";
+    }
+
+    @RequestMapping("/addItem")
+    public String addItem(@ModelAttribute("book") Book book, @ModelAttribute("qty") String qty, Model model,
+            Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        book = bookService.findById(book.getId());
+
+        if (Integer.parseInt(qty) > book.getInStockNumber()) {
+            model.addAttribute("notEnoughStock", true);
+            return "forward:/bookDetail?id=" + book.getId();
+        }
+
+        CartItem cartItem = cartItemService.addBookToCartItem(book, user, Integer.parseInt(qty));
+        model.addAttribute("addBookSuccess", true);
+
+        return "forward:/bookDetail?id=" + book.getId();
     }
 
 }
