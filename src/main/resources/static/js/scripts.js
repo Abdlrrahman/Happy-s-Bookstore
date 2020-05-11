b76fe88f558925a503e1286c7835203533a184fe
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-05-11 23:29:09 +0200

    The same as shipping address logic

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,6 +1,17 @@
+function checkBillingAddress() {
+    if ($("#theSameAsShippingAddress")) {
+        $(".billingAddress").prop("disabled", true);
+    } else {
+        $(".billingAddress").prop("disabled", false);
+    }
+}
+
 $(document).ready(function () {
     $('.cartItemQty').on('change', function () {
         var id = this.id;
         $('#update-item-' + id).css('display', 'inline-block');
     });
+
+    $("#theSameAsShippingAddress").on("click", checkBillingAddress);
+
 });
