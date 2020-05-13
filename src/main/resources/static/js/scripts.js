bf6fdac1e66a98935ad9e175ddac83c919a64073
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-05-13 03:41:58 +0200

    checkPasswordMatch function

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -6,6 +6,24 @@ function checkBillingAddress() {
     }
 }
 
+function checkPasswordMatch() {
+    var password = $("#txtNewPassword").val();
+    var confirmPassword = $("#txtConfirmPassword").val();
+
+    if (password == "" && confirmPassword == "") {
+        $("#checkPasswordMatch").html("");
+        $("#updateUserInfoButton").prop("disabled", false);
+    } else {
+        if (password != confirmPassword) {
+            $("#checkPasswordMatch").html("Passwords do not match!");
+            $("#updateUserInfoButton").prop("disabled", true);
+        } else {
+            $("#checkPasswordMatch").html("Passwords match");
+            $("#updateUserInfoButton").prop("disabled", false);
+        }
+    }
+}
+
 $(document).ready(function () {
     $('.cartItemQty').on('change', function () {
         var id = this.id;
@@ -13,5 +31,7 @@ $(document).ready(function () {
     });
 
     $("#theSameAsShippingAddress").on("click", checkBillingAddress);
+    $("#txtNewPassword").keyup(checkPasswordMatch);
+    $("#txtConfirmPassword").keyup(checkPasswordMatch);
 
 });
