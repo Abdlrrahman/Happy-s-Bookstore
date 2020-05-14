cae7f1443f530a2a8e0c758316837ebe78bfef4b
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-05-14 10:56:25 +0200

    styling and tables

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -31,19 +31,19 @@ public class BookstoreApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		User user1 = new User();
-		user1.setFirstName("John");
-		user1.setLastName("Dow");
-		user1.setUsername("Jon");
-		user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
-		user1.setEmail("JDow@gmail.com");
-		Set<UserRole> userRoles = new HashSet<>();
-		Role role1 = new Role();
-		role1.setRoleId(1);
-		role1.setName("ROLE_USER");
-		userRoles.add(new UserRole(user1, role1));
+		// User user1 = new User();
+		// user1.setFirstName("John");
+		// user1.setLastName("Dow");
+		// user1.setUsername("Jon");
+		// user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
+		// user1.setEmail("JDow@gmail.com");
+		// Set<UserRole> userRoles = new HashSet<>();
+		// Role role1 = new Role();
+		// role1.setRoleId(1);
+		// role1.setName("ROLE_USER");
+		// userRoles.add(new UserRole(user1, role1));
 
-		userService.createUser(user1, userRoles);
+		// userService.createUser(user1, userRoles);
 	}
 
 }
