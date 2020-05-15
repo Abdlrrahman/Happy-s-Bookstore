a3aed2bac1af0e50b3168afadc57ae277cf2557b
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-05-15 10:47:54 +0200

    local scripts fonts and css

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     private static final String[] PUBLIC_MATCHERS = { "/css/**", "/js/**", "/image/**", "/", "/newUser",
-            "/forgetPassword", "/login", "/bookshelf", "/bookDetail/**" };
+            "/forgetPassword", "/login", "/bookshelf", "/bookDetail/**", "/webfonts/**" };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
