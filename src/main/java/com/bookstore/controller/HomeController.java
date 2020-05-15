a2ba162e3bf76f76cf3daeeab4ddae1772a471b8
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-05-15 08:24:37 +0200

    fixed some home controller bugs

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -497,7 +497,6 @@ public class HomeController {
 		model.addAttribute("user", user);
 
 		model.addAttribute("classActiveEdit", true);
-		model.addAttribute("orderList", user.getOrderList());
 
 		return "myProfile";
 	}
@@ -550,6 +549,8 @@ public class HomeController {
 		model.addAttribute("updateSuccess", true);
 		model.addAttribute("user", currentUser);
 		model.addAttribute("classActiveEdit", true);
+		model.addAttribute("listOfShippingAddresses", true);
+		model.addAttribute("listOfCreditCards", true);
 
 		UserDetails userDetails = userSecurityService.loadUserByUsername(currentUser.getUsername());
 
