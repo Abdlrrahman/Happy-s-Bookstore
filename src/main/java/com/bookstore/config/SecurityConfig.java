4338ae8d3ceb112a4ccd53c9c95f35dfb6cc8669
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-05-15 12:46:02 +0200

    styling and access

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     private static final String[] PUBLIC_MATCHERS = { "/css/**", "/js/**", "/image/**", "/", "/newUser",
-            "/forgetPassword", "/login", "/bookshelf", "/bookDetail/**", "/webfonts/**" };
+            "/forgetPassword", "/login", "/bookshelf", "/bookDetail/**", "/webfonts/**", "/faq", "/hours" };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
